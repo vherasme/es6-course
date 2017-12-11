eff2c494ff83d8dabc53f07b05b7dd5f8f3346f0
vherasme <victor.herasme@sogeti.com>
2017-12-11 22:41:46 +0100

    let, const, template literals

--- app/js/index.js
+++ app/js/index.js
@@ -4,15 +4,17 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-import {add, subtract, multiply, division} from './modules/Math';
+import { add, subtract, multiply, division } from './modules/Math'; /* import * from './modules/Math'; */
 import Animal from './modules/Animal';
 
 logTitle('Title');
 /* coding examples */
 
-log("Add function: " + add(4,5));
+log("Add function: " + add(4, 5));
 log("Divide function: " + division(9, 8));
 
 var animal = new Animal('Chip', 10);
 
 log(animal.getName() + " is " + animal.getAge() + " years old");
+log("Template literals: ");
+log(`${animal.getName()} is ${animal.getAge()} years old`)
