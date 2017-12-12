0fe15ac869460be7ddfc61a31ddf9173ba8e96fd
vherasme <victor.herasme@gmail.com>
2017-12-12 17:07:53 +0000

    spread operators

--- app/js/index.js
+++ app/js/index.js
@@ -16,5 +16,42 @@ log("Divide function: " + division(9, 8));
 var animal = new Animal('Chip', 10);
 
 log(animal.getName() + " is " + animal.getAge() + " years old");
-log("Template literals: ");
-log(`${animal.getName()} is ${animal.getAge()} years old`)
+log("\n Template literals: ");
+log(`${animal.getName()} is ${animal.getAge()} years old`);
+
+log("Spread operator arrays: ");
+
+const arrayOne = ['maria', 'josefa', 'juana'];
+const arrayTwo = ['pedro', 'luis', 'jose'];
+
+const concatArray = [...arrayOne, ...arrayTwo];
+
+concatArray.forEach(function(name) {
+    log(name);
+});
+
+const addNumbers = function(x,y,z){
+    return x + y +z;
+}
+
+const numbers = [1, 2, 3];
+
+const addition = addNumbers(...numbers);
+
+log(`Spread operator addition: ${addition}`);
+
+const address = {
+    city: 'LA',
+    country: 'USA',
+    postCode: '33166'
+};
+
+const name = {
+    firstName: 'Andy',
+    lastName: 'Jones'
+};
+
+const person = {...address, ...name};
+
+log('Spread operator with objects: ');
+log(JSON.stringify(person, null, 2));
